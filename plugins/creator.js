630fadf20b924f643c1db4223e30d9bfd972c03a
ShirasuMinima <117260799+ShirasuMinima@users.noreply.github.com>
2022-11-02 17:04:18 +0900

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,12 +20,12 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-*ɴᴀᴍᴇ:* ⓊⒸⒽⒾⒽⒶ ⓃⒶⓇⓊⓉⓄ
-*ᴀɢᴇ:* 20ᵗʰ
+*ɴᴀᴍᴇ:* 七時ムメイ
+*ᴀɢᴇ:* 17ᵗʰ
 *sᴛᴀᴛᴜs:* single 
 
-*ʙɪʀᴛʜᴅᴀʏ:* 21 Des 2002
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Bandung
+*ʙɪʀᴛʜᴅᴀʏ:* 15 March 2005
+*ᴀᴅᴅʀᴇss:* Indonesia, Kalimantan Timur, Balikpapan
 
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
